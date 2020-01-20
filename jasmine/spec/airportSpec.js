c15faa792b3457337fe6b42ae3c70fcd7d4f863e
Umberto Valente <uvalente89@gmail.com>
2020-01-20 16:55:29 +0000

    Added land function to airport

--- jasmine/spec/airportSpec.js
+++ jasmine/spec/airportSpec.js
@@ -1,13 +1,35 @@
 describe("Airport", function() {
   var airport;
+  var plane;
 
   beforeEach(function() {
     airport = new Airport();
+    plane = new Plane();
   });
 
   describe("set the default capacity", function() {
     it("set the default capacity to 20", function() {
       expect(airport.capacity).toEqual(DEFAULT_CAPACITY);
+    });
+
+    it('set the capacity to a new value', function() {
+      airport.capacity = 6;
+      expect(airport.capacity).toEqual(6);
+    });
+  });
+
+  describe('can land a plane', function() {
+    it('if the capacity is not full', function() {
+      airport.land(plane)
+      expect(airport.apron.length).toEqual(1)
+    });
+
+    it('can land multiple planes', function() {
+      airport.land(plane)
+      airport.land(plane)
+      airport.land(plane)
+      expect(airport.apron.length).toEqual(3)
+      expect(airport.apron).toEqual([plane, plane, plane])
     });
   });
 });
--- jasmine/src/airport.js
+++ jasmine/src/airport.js
@@ -2,4 +2,9 @@ const DEFAULT_CAPACITY = 20;
 
 function Airport() {
   this.capacity = DEFAULT_CAPACITY;
+  this.apron = []
+}
+
+Airport.prototype.land = function(plane) {
+  this.apron.push(plane);
 }
